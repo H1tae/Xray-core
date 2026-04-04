af9894175e69dd1f68589a19eb1ec25ae404328b
H1tae <H1tae@yandex.ru>
2026-04-04 21:43:53 +0300

    another dir for deploy

--- deploy/deploy_allocated/logs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# shellcheck disable=SC1091
-source "${script_dir}/common.sh"
-
-xray_deploy_logs "${script_dir}" "${1:-${script_dir}/.env}"
--- deploy/deploy_shared/build_image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# shellcheck disable=SC1091
-source "${script_dir}/common.sh"
-
-xray_deploy_build_image "${script_dir}"
