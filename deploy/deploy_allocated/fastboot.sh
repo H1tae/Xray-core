501bff17cfd625c619921fa247b8be5f7f03b070
H1tae <H1tae@yandex.ru>
2026-04-04 21:43:53 +0300

    another dir for deploy

--- deploy/deploy_allocated/fastboot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-env_file="${1:-${script_dir}/.env}"
-
-bash "${script_dir}/bootstrap_server.sh" "${env_file}"
